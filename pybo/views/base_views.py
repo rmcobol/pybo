d5dba1d8c32f9218b2a75a3f384d030fd8434806
rmcobol1 <lstrmcobol123@naver.com>
2023-11-07 18:56:38 +0900

    파이보에서 직접 로고 출력

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
-
+    logger.info("INFO 레베로 출력")
     """
     pyboy 목록 출력
     """
